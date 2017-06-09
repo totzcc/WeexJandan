305f45490f8ba1f3ab38278390885f419b84c6a0
totzcc <totzcc@163.com>
2017-06-09 15:04:06 +0800

    add android 打包脚本

---
+++ platforms/android/android.sh
@@ -0,0 +1,2 @@
+curl "https://coding.net/u/totzcc/p/appBuildScript/git/raw/master/android.py" -o android.py
+python android.py
--- platforms/android/pack.sh
+++
@@ -1,2 +0,0 @@
-curl https://coding.net/u/totzcc/p/appBuildScript/git/raw/master/pack.py -o pack.py
-python pack.py
