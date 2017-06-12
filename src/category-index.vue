dba85a59ad24be4b2ae8a7661408e54eba7f941a
totzcc <totzcc@163.com>
2017-06-12 10:08:54 +0800

    update 订阅管理UI

--- src/category-index.vue
+++ src/category-index.vue
@@ -16,25 +16,24 @@
 				</div>
 				<div class="container-item">
 					<div v-for="category in subscribes.categorys">
-						<div class="item" :category="category" @click="remove" style="background-color: #FFFFFF;">
+						<div class="item" :category="category" @click="remove">
 							<text>{{category}}</text>
 						</div>
 						<image v-if="isSubscribeing" style="position: absolute; left: 0; top: 0; width: 40; height: 40;" :src="config.image('delete.png')"/>
 					</div>
 				</div>
 			</div>
-			<div v-for="item in datalist">
+			<div v-for="item in datalist" v-if="!item.hideCategory">
 				<div class="header">
 					<text>{{item.title}}</text>
 				</div>
 				<div class="container-item">
-					<div v-for="category in item.categorys">
-						<div class="item" :category="category" @click="click" :style="{backgroundColor: category.isSubscribe && isSubscribeing ? '#e3e3e3' : '#ffffff'}">
+					<div v-for="category in item.categorys" v-if="!category.isSubscribe">
+						<div class="item" :category="category" :item="item" @click="click">
 							<text>{{category.title}}</text>
 						</div>
 						<image v-if="isSubscribeing" style="position: absolute; left: 0; top: 0; width: 40; height: 40;" :src="config.image('add.png')"/>
 					</div>
-					
 				</div>
 			</div>
 		</scroller>
@@ -44,13 +43,13 @@
 	.container{background-color: #f3f3f3; flex:1;}
 	.header{height: 100px; width: 750px; justify-content: center; align-items: center;}
 	.container-item{flex-direction: row;flex-wrap: wrap;}
-	.item{justify-content: center; align-items: center; margin: 10px; width: 229px; height: 88px;}
+	.item{background-color: #FFFFFF; justify-content: center; align-items: center; margin: 10px; width: 229px; height: 88px;}
 	.item-image{background-color: #e3e3e3;flex: 1; height:200px}
 	.item-text{padding: 10px; color: #333333;}
 	.loading{width: 750px; padding-top: 40px; padding-bottom: 40px; align-items: center;justify-content: center;}
 </style>
 <script>
-	const navigator = weex.requireModule('navigator')
+	let navigator = weex.requireModule('navigator')
 	import config from './config'
 	import jandan from './services/jandan'
 	module.exports = {
@@ -103,39 +102,47 @@
 		},
 		methods:{
 			click(e){
-				const category = e.target.attr.category
+				let category = e.target.attr.category
+				let item = e.target.attr.item
+				if(!category.title){
+					return
+				}
 				if(this.isSubscribeing) {
 					category.isSubscribe = true
 					if (this.subscribes.categorys.indexOf(category.title) == -1) {
 						this.subscribes.categorys.push(category.title)
 					}
 				} else {
-					const url = config.js('category-list.js') + "?category=" + encodeURI(category.title)
+					let url = config.js('category-list.js') + "?category=" + encodeURI(category.title)
 					navigator.push({url:url},()=>{})
 				}
 			},
 			remove(e){
-				const category = e.target.attr.category
-				let index = this.subscribes.categorys.indexOf(category)
-				this.subscribes.categorys.splice(index, 1)
-				this.resetCategoryState(category,false)
+				if(this.isSubscribeing) {
+					let category = e.target.attr.category
+					let index = this.subscribes.categorys.indexOf(category)
+					this.subscribes.categorys.splice(index, 1)
+					this.setCategoryState(category,false)
+				} else {
+					let category = e.target.attr.category
+					let url = config.js('category-list.js') + "?category=" + encodeURI(category)
+					navigator.push({url:url},()=>{})
+				}
 			},
 			refreshCategoryState(){
 				this.subscribes.categorys.forEach((category) => {
-					this.resetCategoryState(category)
+					this.setCategoryState(category)
 				})
 			},
-			resetCategoryState(category,isSubscribe) {
+			setCategoryState(category,isSubscribe) {
 				if(isSubscribe == undefined) {
 					isSubscribe = true
 				}
 				for(var i = 0; i < this.datalist.length; i ++) {
 					let categorys = this.datalist[i].categorys;
 					for(var j = 0; j < categorys.length; j ++) {
-						console.log(categorys[j].title)
 						if(category == categorys[j].title) {
 							categorys[j].isSubscribe = isSubscribe
-							console.log('categorys[j].isSubscribe=' + categorys[j].isSubscribe)
 							return
 						}
 					}
