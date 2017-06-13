dface451682cac0aeb44d323001c1fe6fea28002
totzcc@163.com <totzcc@163.com>
2017-06-13 22:25:45 +0800

    add 接受通知

--- platforms/android/app/src/main/java/android/jandan/totzcc/com/weexjandan/MessageReceiver.java
+++ platforms/android/app/src/main/java/android/jandan/totzcc/com/weexjandan/MessageReceiver.java
@@ -1,8 +1,42 @@
 package android.jandan.totzcc.com.weexjandan;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.alibaba.sdk.android.push.notification.CPushMessage;
+
+import java.util.Map;
+
 /**
  * Created by Administrator on 2017/6/12.
  */
 
 public class MessageReceiver extends com.alibaba.sdk.android.push.MessageReceiver {
+    // 消息接收部分的LOG_TAG
+    public static final String REC_TAG = "receiver";
+    @Override
+    public void onNotification(Context context, String title, String summary, Map<String, String> extraMap) {
+        // TODO 处理推送通知
+        Log.e("MyMessageReceiver", "Receive notification, title: " + title + ", summary: " + summary + ", extraMap: " + extraMap);
+    }
+    @Override
+    public void onMessage(Context context, CPushMessage cPushMessage) {
+        Log.e("MyMessageReceiver", "onMessage, messageId: " + cPushMessage.getMessageId() + ", title: " + cPushMessage.getTitle() + ", content:" + cPushMessage.getContent());
+    }
+    @Override
+    public void onNotificationOpened(Context context, String title, String summary, String extraMap) {
+        Log.e("MyMessageReceiver", "onNotificationOpened, title: " + title + ", summary: " + summary + ", extraMap:" + extraMap);
+    }
+    @Override
+    protected void onNotificationClickedWithNoAction(Context context, String title, String summary, String extraMap) {
+        Log.e("MyMessageReceiver", "onNotificationClickedWithNoAction, title: " + title + ", summary: " + summary + ", extraMap:" + extraMap);
+    }
+    @Override
+    protected void onNotificationReceivedInApp(Context context, String title, String summary, Map<String, String> extraMap, int openType, String openActivity, String openUrl) {
+        Log.e("MyMessageReceiver", "onNotificationReceivedInApp, title: " + title + ", summary: " + summary + ", extraMap:" + extraMap + ", openType:" + openType + ", openActivity:" + openActivity + ", openUrl:" + openUrl);
+    }
+    @Override
+    protected void onNotificationRemoved(Context context, String messageId) {
+        Log.e("MyMessageReceiver", "onNotificationRemoved");
+    }
 }
